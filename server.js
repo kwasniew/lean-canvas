9d3db621c0db2aa9c5a856bcb03c502ba8ad46a4
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-12-02 19:11:36 +0100

    stub http server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/canvas/:id', function(req, res, next) {
+  res.send('get');
+});
+
+app.post('/canvas', function(req, res, next) {
+  res.send('post');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!');
+});
