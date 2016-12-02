6e4ab9d79d050add697c0b3bdb52e9cb834b2bf1
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-12-02 21:02:20 +0100

    sending full model to backend

--- LeanCanvas.elm
+++ LeanCanvas.elm
@@ -64,6 +64,27 @@ modelToJson model =
         , ( "name", JE.string model.name )
         , ( "editing", JE.bool model.editing )
         , ( "oldName", JE.string model.name )
+        , ( "cards"
+          , JE.list
+                (List.map
+                    (\card ->
+                        JE.object
+                            [ ( "section", JE.string card.section )
+                            , ( "text", JE.string card.text )
+                            , ( "id", JE.int card.id )
+                            , ( "editing", JE.bool card.editing )
+                            ]
+                    )
+                    model.cards
+                )
+          )
+        , ( "entryCard"
+          , JE.object
+                [ ( "section", JE.string model.entryCard.section )
+                , ( "text", JE.string model.entryCard.text )
+                , ( "id", JE.int model.entryCard.id )
+                ]
+          )
         ]
         |> JE.encode 4
 
