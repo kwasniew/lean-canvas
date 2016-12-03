8896e39c7636fc63f066e44d94846ae576c6cb3b
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-12-03 13:58:27 +0100

    simple saving on the server side

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var uuid = require('node-uuid');
 
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '10kb'}));
 
 app.use(express.static('public'));
 
+var db = {};
+
 app.get('/canvas/:id', function(req, res, next) {
-  res.send('get');
+  if(db[req.params.id]) {
+    res.json(db[req.params.id]);
+  }
+  res.status(404);
 });
 
 app.post('/canvas', function(req, res, next) {
-  console.log(req.body);
-  res.send('post');
+  req.body.id = uuid.v4();
+  db[req.body.id] = req.body;
+
+  res.send('saved ' + req.body.id);
 });
 
 app.listen(process.env.PORT || 3000, function () {
