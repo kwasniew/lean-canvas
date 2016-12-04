12fe94a4eb3ef83cae994c9d762d41433d30639c
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-12-04 20:04:30 +0100

    card reordering tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,33 +2,43 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import String
 import LeanCanvas exposing (Card, Move, reorderCards)
+
+
+expectReordered : List Int -> Move -> List Int -> Expect.Expectation
+expectReordered input move output =
+    let
+        card id =
+            Card "section" (toString id) id False
+
+        cards =
+            List.map card input
+
+        expectedReordered =
+            List.map card output
+    in
+        Expect.equal (reorderCards cards move) expectedReordered
 
 
 all : Test
 all =
     describe "Card Reordering"
-        [ test "two items" <|
+        [ test "two items - move up" <|
             \() ->
-                let
-                    card1 =
-                        Card "section" "a" 100 False
-
-                    card2 =
-                        Card "section" "b" 101 False
-
-                    cards =
-                        [ card1, card2 ]
-
-                    move =
-                        Move 100 0 1
-                in
-                    Expect.equal (reorderCards cards move) [ card2, card1 ]
-        , test "String.left" <|
+                expectReordered [ 100, 200 ] (Move 100 0 1) [ 200, 100 ]
+        , test "three items - move up by one" <|
             \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-          -- , test "This test should fail" <|
-          --     \() ->
-          --         Expect.fail "failed as expected!"
+                expectReordered [ 100, 200, 300 ] (Move 100 0 1) [ 200, 100, 300 ]
+        , test "three items - move up by two" <|
+            \() ->
+                expectReordered [ 100, 200, 300 ] (Move 100 0 2) [ 200, 300, 100 ]
+        , test "two items - move down" <|
+            \() ->
+                expectReordered [ 100, 200 ] (Move 200 1 0) [ 200, 100 ]
+        , test "three items - move down by one" <|
+            \() ->
+                expectReordered [ 100, 200, 300 ] (Move 300 2 1) [ 100, 300, 200 ]
+        , test "three items - move down by two" <|
+            \() ->
+                expectReordered [ 100, 200, 300 ] (Move 300 2 0) [ 300, 100, 200 ]
         ]
