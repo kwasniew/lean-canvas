b21618255cd4d87a5bf782a64b376d6e1bff908f
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-12-04 18:00:58 +0100

    first real reordering test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,18 +3,32 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
+import LeanCanvas exposing (Card, Move, reorderCards)
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
+    describe "Card Reordering"
+        [ test "two items" <|
             \() ->
-                Expect.equal (3 + 7) 10
+                let
+                    card1 =
+                        Card "section" "a" 100 False
+
+                    card2 =
+                        Card "section" "b" 101 False
+
+                    cards =
+                        [ card1, card2 ]
+
+                    move =
+                        Move 100 0 1
+                in
+                    Expect.equal (reorderCards cards move) [ card2, card1 ]
         , test "String.left" <|
             \() ->
                 Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+          -- , test "This test should fail" <|
+          --     \() ->
+          --         Expect.fail "failed as expected!"
         ]
